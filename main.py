ac9b98f5820f90aa911de04ecda5f36db082ee8d
suhaib <suhaibvbm@gmail.com>
2023-11-04 11:39:03 +0530

    Relative file path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-with open("/Users/Administrator/Desktop/new_file.txt") as file:
+with open("../../Desktop/new_file.txt") as file:
     content = file.read()
     print(content)
-
 #1) move new_file.txt to Desktop finding path
+# 2) Going to backward from our working file to txt

15fa4007a5d07af2c4426410529f3e73b0561b20
suhaib <suhaibvbm@gmail.com>
2023-11-04 11:34:16 +0530

    absolute file path

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-with open("new_file.txt") as file:
+with open("/Users/Administrator/Desktop/new_file.txt") as file:
     content = file.read()
     print(content)
+
+#1) move new_file.txt to Desktop finding path
